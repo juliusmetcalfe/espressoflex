cceb02ea5438fc429675867d5271057c558a79bb
Julius Metcalfe <julius.metcalfe@gmail.com>
2019-01-16 15:00:49 +1000

    build.sh: Continued dev, WIP update

--- build.sh
+++ build.sh
@@ -4,13 +4,22 @@
 export CROSS_COMPILE=aarch64-linux-gnu-
 export ARCH=arm64
 
+export TOPDIR=$(pwd)
+
+build_buildroot () {
+    (
+        cd $BUILDROOT_DIR
+        cp ../br_espressoconfig .config
+        make
+    )
+}
 
 build_uboot () {
     (
         cd $UBOOT_DIR
         make mvebu_espressobin-88f3720_defconfig
         cp ../uboot_espressoconfig .config
-        make DEVICE_TREE=armada-3720-espressobin
+        make -j$(nproc) DEVICE_TREE=armada-3720-espressobin
         if [ ! -f u-boot.bin ]; then
             echo "Building u-boot failed"
             exit;
@@ -21,36 +30,35 @@ build_uboot () {
 
 build_linux () {
     (
+        export ARCH=arm64
+        export CROSS_COMPILE=aarch64-linux-gnu-
         cd $LINUX_DIR
-        make mvebu_espressobin-88f3720_defconfig
-        cp ../uboot_espressoconfig .config
-        make DEVICE_TREE=armada-3720-espressobin
-        if [ ! -f u-boot.bin ]; then
-            echo "Building u-boot failed"
-            exit;
-        fi  
-    )   
-    export BL33=$UBOOT_DIR/u-boot.bin
+        make defconfig
+        cp ../linux_espressoconfig .config
+        make -j$(nproc) Image dtbs modules
+        mkdir $TOPDIR/rootfs
+        make modules_install INSTALL_MOD_PATH=$TOPDIR/rootfs/
+    )
 }
 
 
-if [ ! -d ./Buildroot ]; then
+if [ ! -d ./buildroot ]; then
     echo "Pulling Buildroot-2018.02.x..."
     (
         git clone git://git.busybox.net/buildroot -b 2018.02.x buildroot --depth=1
     )
 fi
-BUILDROOT_DIR=$(pwd)/buildroot/
+BUILDROOT_DIR=$(pwd)/buildroot
 
 
 if [ ! -d ./linux ]; then
     echo "Pulling Linux-4.19.y..."
     (
-        git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git -b linux-4.9.y --depth=1 linux
+        git clone git://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git -b linux-4.19.y --depth=1 linux
         git am ../espressopatches/linux/*.patch
     )
 fi
-LINUX_DIR=$(pwd)/linux/
+LINUX_DIR=$(pwd)/linux
 
 
 if [ ! -d ./u-boot ]; then
@@ -60,7 +68,7 @@ if [ ! -d ./u-boot ]; then
         git am ../espressopatches/u-boot/*.patch
     )
 fi
-UBOOT_DIR=$(pwd)/u-boot/
+UBOOT_DIR=$(pwd)/u-boot
 
 if [ ! -d ./atf ]; then
     echo "Pulling atf..."
@@ -68,7 +76,7 @@ if [ ! -d ./atf ]; then
         git clone https://github.com/MarvellEmbeddedProcessors/atf-marvell.git atf
     )
 fi
-ATF_DIR=$(pwd)/atf/
+ATF_DIR=$(pwd)/atf
 
 if [ ! -d ./A3700-utils ]; then
     echo "Pulling A3700-utils..."
@@ -76,10 +84,11 @@ if [ ! -d ./A3700-utils ]; then
         git clone https://github.com/MarvellEmbeddedProcessors/A3700-utils-marvell.git A3700-utils
     )
 fi
-A3700UTILS_DIR=$(pwd)/A3700-utils/
+A3700UTILS_DIR=$(pwd)/A3700-utils
 
 
-
+build_linux;
+build_buildroot;
 build_uboot;
 
 
